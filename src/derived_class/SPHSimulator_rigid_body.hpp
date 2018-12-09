ba9df00730d2cb6c79018837388e7c65656a03db
Chui <kmchao33@gmail.com>
2018-12-09 12:08:46 +0100

    PBF with BUGGGGs

--- src/derived_class/SPHSimulator_rigid_body.hpp
+++ src/derived_class/SPHSimulator_rigid_body.hpp
@@ -2,13 +2,14 @@
 #define SPHSIMULATOR_RIGID_BODY_H
 #include "SPHSimulator.hpp"
 
+#include <cstdlib>
+
+#define WCSPH 0
+#define PBFSPH 1
 
 class SPHSimulator_rigid_body : public SPHSimulator
 {
 public:
-	bool viscosity_flag = true;
-	bool XSPH_flag = true;
-
     SPHSimulator_rigid_body(int N,  Real uParticle_len, Real dt, Real eta, Real B, Real alpha, Real rest_density, int with_viscosity, int with_XSPH)
 	 : SPHSimulator(N, uParticle_len,dt, eta, B, alpha, rest_density)
 	{
@@ -26,27 +27,43 @@ public:
 
     virtual void update_simulation() override
     {
-        //float fr = 2.4/N * 2 ;
-        //set_neighbor_search_radius(fr);
+    	switch(solver_type)
+    	{
+    		case WCSPH:
+    			update_simulation_WCSPH();
+    			break;
+    		case PBFSPH:
+    			update_simulation_PBFSPH();
+    			break;
+    		default:
+    			std::cout << "Error: unknown solver type" << std::endl;
+    	        std::exit(-1);
+    	}
+    }
 
+    //virtual void generate_particles() = 0;
+
+protected:
+    // should I also move all boundary related function to here? Or maybe not...
+	bool viscosity_flag = true;
+	bool XSPH_flag = true;
+
+	int solver_type = 1;
+	int epoch = 5;
+
+	void update_simulation_WCSPH()
+	{
         std::vector< std::vector<size_t> > neighbors_set = neighborSearcher.find_neighbors_within_radius(true);
         std::vector< std::vector<size_t> > neighbors_in_boundary = neighborSearcher.find_neighbors_in_boundary( );
 
         Real r = neighbor_search_radius;
         std::vector<Real> densities = particleFunc.update_density(neighbors_set, neighbors_in_boundary, particles, boundary_particles, r);
-        //std::vector<Real> densities = particleFunc.update_density(neighbors_set, particles, r);
-
-        //Real water_rest_density = 1000.0;
-        //Real B = 100.0;
 
         std::vector<RealVector3> external_forces;
         for (size_t i=0; i<particles.size(); ++i)
-            //external_forces.push_back( RealVector3(0.0, 0.0, -0.981 * particles[i].mass) ); //Neng: we have the gravity in class private
             external_forces.push_back( gravity * particles[i].mass ); //Neng: we have the gravity in class private
 
         particleFunc.update_acceleration( particles, boundary_particles, neighbors_set, neighbors_in_boundary, densities, external_forces, r, viscosity_flag);
-        //particleFunc.update_acceleration( particles, neighbors_set, densities, external_forces, water_rest_density, r, B);
-
 
         if (XSPH_flag == false)
         {
@@ -58,14 +75,152 @@ public:
             particleFunc.update_position(particles, dt, neighbors_set, r, densities);
         }
 
-        update_positions();;
-    }
+        update_positions();
+	}
 
-    //virtual void generate_particles() = 0;
+	void update_simulation_PBFSPH()
+	{
+		// Step 0: save the current position information before any updates
+		std::vector<RealVector3> old_positions;
+		for (auto& pos : positions)
+		{
+			old_positions.push_back(RealVector3(pos));
+		}
 
-protected:
-    // should I also move all boundary related function to here? Or maybe not...
+		// Step 1: preview of particles's status
+    	for (size_t i=0; i<particles.size(); ++i)
+		{
+    		particles[i].velocity += gravity * dt;
+		}
+        particleFunc.update_position(particles, dt);
+        update_positions();
 
+        // Step 2: search neighbors
+        std::vector< std::vector<size_t> > neighbors_set = neighborSearcher.find_neighbors_within_radius(true);
+        std::vector< std::vector<size_t> > neighbors_in_boundary = neighborSearcher.find_neighbors_in_boundary( );
+
+        // Step 3: iteration of lambda and position computing
+        Real r = neighbor_search_radius;
+        std::vector<Real> densities = particleFunc.update_density(neighbors_set, neighbors_in_boundary, particles, boundary_particles, r);
+
+        for (int itr=0; itr<epoch; ++itr)
+        {
+        	// Step 3.1: compute lambda
+        	std::vector<Real> lambda; // only contain lambda of fluid particles, cuz lambda
+        	for (size_t i=0; i<particles.size(); ++i)
+        	{
+        		mParticle P_i = particles[i];
+
+        		size_t number_of_fluid_neighbors_of_i = neighbors_set[i].size();
+        		size_t number_of_boundary_neighbors_of_i = neighbors_in_boundary[i].size();
+
+        		Real C_i = densities[i] / rest_density - 1;
+        		std::vector<RealVector3> grad_of_C_i; // vector with size #fluid_neighbors + #boundary_neighbors
+
+        		Real S_i = 0.0;
+
+        		// compute grad_of_C_i and S_i
+        		for (size_t k=0; k<(number_of_fluid_neighbors_of_i+number_of_boundary_neighbors_of_i); ++k)
+        		{
+        			size_t j;
+        			mParticle NP_ij;
+
+        			if (k == 0) // that is, j == i
+        			{
+        				// skip first, will later compute it by adding up all other gradients
+        				grad_of_C_i.push_back(RealVector3(0.0, 0.0, 0.0));
+        				continue;
+        			}
+        			else if (k < number_of_fluid_neighbors_of_i) // j != i and j is fluid neighbor
+        			{
+            			j = neighbors_set[i][k];
+            			NP_ij = particles[j];
+           			}
+        			else { // j != i and j is boundary neighbor
+           				j = neighbors_in_boundary[i][k-number_of_fluid_neighbors_of_i];
+           				NP_ij = boundary_particles[j];
+            		}
+
+					RealVector3 grad_W = kernelHandler.gradient_of_kernel( P_i.position, NP_ij.position, 4 );
+					RealVector3 grad_Cij = (-NP_ij.mass / rest_density) * grad_W;
+
+					grad_of_C_i.push_back(grad_Cij);
+
+					// subtract it with grad_Cii
+					grad_of_C_i[0] -= grad_Cij;
+
+					// update S_i, but here got to skip C_ii
+					S_i += (grad_Cij[0]*grad_Cij[0] + grad_Cij[1]*grad_Cij[1] + grad_Cij[2]*grad_Cij[2]) / NP_ij.mass;
+        		}
+
+        		// add C_ii to complete S_i
+        		RealVector3 grad_Cii = grad_of_C_i[0];
+        		S_i += (grad_Cii[0]*grad_Cii[0] + grad_Cii[1]*grad_Cii[1] + grad_Cii[2]*grad_Cii[2]) / P_i.mass;
+
+        		// compute lambda_i and push back
+        		Real lambda_i = 0.0;
+
+        		if (C_i > 0.0)
+        		{
+        			lambda_i = -C_i / (S_i + 0.0001);
+        		}
+
+        		lambda.push_back(lambda_i);
+        	}
+
+        	// Step 3.2: correct position
+        	for (size_t i=0; i<particles.size(); ++i)
+        	{
+        		Real lambda_i = lambda[i];
+        		mParticle P_i = particles[i];
+        		RealVector3 dx_i = RealVector3(0.0, 0.0, 0.0);
+
+        		size_t number_of_fluid_neighbors_of_i = neighbors_set[i].size();
+        		size_t number_of_boundary_neighbors_of_i = neighbors_in_boundary[i].size();
+
+        		// sum up the contribution of all fluid and boundary neighbors
+        		for (size_t k=0; k<(number_of_fluid_neighbors_of_i+number_of_boundary_neighbors_of_i); ++k)
+        		{
+        			size_t j;
+        			mParticle NP_ij;
+        			Real lambda_j;
+
+        			if (k == 0) // that is, j == i
+        			{
+        				NP_ij = P_i;
+        				lambda_j = lambda_i;
+        			}
+        			else if (k < number_of_fluid_neighbors_of_i) // j != i and j is fluid neighbor
+        			{
+            			j = neighbors_set[i][k];
+            			NP_ij = particles[j];
+            			lambda_j = lambda[k];
+           			}
+        			else { // j != i and j is boundary neighbor
+           				j = neighbors_in_boundary[i][k-number_of_fluid_neighbors_of_i];
+           				NP_ij = boundary_particles[j];
+        				lambda_j = lambda_i; // <----- idea from the assignment sheet, not quite understand though
+            		}
+
+					RealVector3 grad_W_ij = kernelHandler.gradient_of_kernel( P_i.position, NP_ij.position, 4 );
+
+        			dx_i += (lambda_i * NP_ij.mass / P_i.mass + lambda_j) * grad_W_ij;
+        		}
+
+        		// computing of dx_i completed here
+        		dx_i /= rest_density;
+
+        		// correct position x_i
+        		particles[i].position += dx_i;
+
+        		// Step 4: update velocity
+        		particles[i].velocity = (particles[i].position - old_positions[i]) / dt;
+
+        		update_positions();
+        	}
+        }
+
+	}
 };
 
 #endif // SPHSIMULATOR_RIGID_BODY_H
